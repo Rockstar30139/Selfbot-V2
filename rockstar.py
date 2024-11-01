e85526c2950e42ecaf99ca25bda23f74cbb9b8a6
Rockstar30139 <147255392+Rockstar30139@users.noreply.github.com>
2024-11-01 21:14:17 +0530

    Update rockstar.py

--- rockstar.py
+++ rockstar.py
@@ -81,15 +81,16 @@ ROCKSTAR SELFBOT V1.1
 async def SUPPORT(ctx):
        await ctx.send("""https://discord.gg/A3hdPqu9GY"""
 
-@ok.command(aliases=['general', 'g'])
-async def GENERAL(ctx):
-       await ctx.send("""**```yml
+@ok.command(name="generalhelp", aliases=["gh"])
+async def general_help(ctx):
+    mess = ("""**```yml
 ROCKSTAR SELFBOT V1.1 
 ``````js
 R O C K S T A R  S E L F B O T
 
 prune, mc, ban, kick, mute, ping, calc, asci, mujra, dmall, leave, getbal, purge, avatar, define, boosts, massmail, connectvc, ltcprice, gayrate, loverate, userinfo, copyserver, change_hypesquad, serverinfo, spam, status, stopstatus, rockstarop, hack2, cum, fm (first message), slots, autobuy, gituser, gitsearch, checkpromo (promo link), i2c, minesweeper, uptime, _100, leaveall (Leave all guilds), guildsid, randomip, leaveguild (Leaves a single guild), abuse, c2i, link``````yml
-!   ROCKSTAR SELFBOT    !```**"""
+!   ROCKSTAR SELFBOT    !```**""")
+    await ctx.send(mess)
 
 @ok.command()
 async def prune(ctx: commands.Context):
